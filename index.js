3e5e8754d8cd9f3b8f3127e7b3e592b94243cb60
Vincentfrg <vferrag78@gmail.com>
2023-03-25 19:57:29 +0100

    setup NodeJS and discord package to start

---
+++ index.js
@@ -0,0 +1,7 @@
+const { CommandoClient } = require('discord.js-commando');
+
+const client = new CommandoClient({
+    commandPrefix: '-',
+    owner: '1089254020440408155',
+    invite: 'https://discord.gg/mXErwAdk'
+});
